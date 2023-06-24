362ae320ce2f8da6507183523a74299f62ade438
userariii <abhattacharjee717@gmail.com>
2023-06-24 17:49:34 +0000

    sdm660-common: Inherit several Android Go configurations

--- sdm660.mk
+++ sdm660.mk
@@ -428,6 +428,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Trust
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
